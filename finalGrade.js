be9e9b7254104ce57bf53b4aca1e1f9bc19bf3d2
Deborah <deborahmdsc@gmail.com>
2022-08-24 14:05:17 +1000

    Creating a function for calculating the final grade

---
+++ finalGrade.js
@@ -0,0 +1,26 @@
+
+const finalGrade = (midterm, final, homework) => {
+  if((midterm < 0 || midterm > 100) || 
+     (final < 0 || final > 100) || 
+     (homework < 0 || homework > 100)) {
+
+    return 'You have entered an invalid grade.';
+  }
+
+  const average = (midterm + final + homework) / 3
+
+  if(average <= 59) {
+    return 'E';
+  } else if (average >= 60 && average <= 69) {
+    return 'D';
+  } else if (average >= 70 && average <= 79) {
+    return 'C';
+  } else if (average >= 80 && average <= 89) {
+    return 'B';
+  } else if (average >= 90 && average <= 100) {
+    return 'A';
+  }
+  
+}
+
+console.log(finalGrade(69, 72, 100));
